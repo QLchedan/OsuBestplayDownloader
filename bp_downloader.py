ab34e1ca82044dcbed4126dcf374560d743412e9
QLchedan <45850458+QLchedan@users.noreply.github.com>
2023-03-26 11:26:13 +0800

    Update bp_downloader.py

--- bp_downloader.py
+++ bp_downloader.py
@@ -1,16 +1,18 @@
+# -*- coding: utf-8 -*-
 from PySide6.QtWidgets import *
 from PySide6.QtCore import QFile, QTimer
-from PySide6.QtUiTools import QUiLoader
+#from PySide6.QtUiTools import QUiLoader
 from osu_apy_v2 import OsuApiV2
 from _thread import start_new_thread
 from mainui import Ui_MainWindow
 from oauth_configui import Ui_Dialog
-from io import BytesIO
+from shutil import copyfile
+import webbrowser
+import tempfile
 import time
 import os
-import requests
-import zipfile
 import re
+import subprocess
 
 class SetAuthWindow(QDialog):
     def __init__(self):
@@ -20,6 +22,7 @@ class SetAuthWindow(QDialog):
         self.ui.setupUi(self)
         self.setWindowTitle('OSU BP下载器')
         self.ui.apply.clicked.connect(self.set_oauth)
+        self.ui.pushButton.clicked.connect(self.open_gh)
 
     def set_oauth(self):
         client_id = self.ui.lineEdit.text()
@@ -28,13 +31,18 @@ class SetAuthWindow(QDialog):
             f.write(client_id + '\n' + secret)
         w.init_osu_api()
 
+    def open_gh(self):
+        webbrowser.open_new('https://github.com/QLchedan/OsuBestplayDownloader') 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-
+        self.bp = [0]
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.saw = None
+        self.tasks = 0
+        self.is_downloading = False
         self.ui.progressBar.setValue(0)
         self.setWindowTitle('OSU BP下载器')
         self.ui.get_bp_button.clicked.connect(self.get_bp)
@@ -44,6 +52,7 @@ class MainWindow(QMainWindow):
         self.ui.download_all_button.clicked.connect(self.start_download)
         self.ui.progressBar.setRange(0, 100)
         self.done = 0
+        self.t = 0
         self.timer = QTimer()
         self.timer.start(1000)
         self.timer.timeout.connect(self.progress_display)
@@ -57,6 +66,10 @@ class MainWindow(QMainWindow):
         if len(oauth) != 2:
             oauth = -1
         return oauth
+    
+    def closeEvent(self, event):
+        os.system('taskkill /F /IM aria2c.exe')
+        event.accept()
 
     def init_osu_api(self):
         oauth = self.init_oauth()
@@ -106,8 +119,7 @@ class MainWindow(QMainWindow):
     def set_oauth(self):
         if self.saw is None:
             self.saw = SetAuthWindow()
-        self.saw.show()
-        
+        self.saw.show()       
 
     def sel_osz_path(self):
         file_path = QFileDialog.getExistingDirectory(
@@ -126,49 +138,68 @@ class MainWindow(QMainWindow):
         self.ui.save_to_osu_textedit.setText(file_path)
 
     def start_download(self):
+        self.t = time.time()
+        self.is_downloading = True
         if self.ui.down_from_sayo.isChecked():
+            self.ui.down_status_label.setText('下载中')
             self.done = 0
             start_new_thread(self.downloading, (0,))
-            #start_new_thread(self.progress_display, (0,))
 
     def downloading(self, n):
         self.down_from_sayo = self.ui.down_from_sayo.isChecked()
-        for k in range(len(self.bp)):
-            start_new_thread(self.download, (k,))
-            time.sleep(2)
+        bp = list(set(self.bp))
+        k = 0
+        while True:
+            if k - self.done <= 4:
+                for i in range(5):
+                    self.tasks += 1
+                    start_new_thread(self.download, (k,))
+                    k += 1
+            if self.done == len(bp):
+                break
         
 
     def download(self, i):
         if self.ui.down_from_sayo.isChecked():
-            map_id = self.bp[i]
-            if len(str(map_id)) >= 5:
-                path = str(map_id)[0:-4] + '/' + str(map_id)[-4:len(str(map_id))]
-            else:
-                path = '0/' + str(map_id)
-            filename = str(map_id) + ' ' + self.composer[i] + ' - ' + self.songname[i]
-            filename = re.sub('[\/:*?"<>|]', '', filename)
-            if self.done % 3 == 0:
-                file = requests.get('https://tc2.sayobot.cn:25225/beatmaps/' + path + '/novideo').content
-            elif self.done % 3 == 1:
-                file = requests.get('https://b4.sayobot.cn:25225/beatmaps/' + path + '/novideo').content
-            else:
-                file = requests.get('https://b11.sayobot.cn:25225/beatmaps/' + path + '/novideo').content
-            if self.ui.save_as_osz_checkbox.isChecked():
-                with open(self.ui.save_as_osz_textedit.text() + '/' + filename + '.osz', 'wb+') as f:
-                    f.write(file)
-            if self.ui.save_to_osu_checkbox.isChecked():
-                zip_file = zipfile.ZipFile(BytesIO(file))
-                if os.path.exists(self.ui.save_to_osu_textedit.text()) and not os.path.exists(self.ui.save_to_osu_textedit.text() + '/' + filename):
-                    os.mkdir(self.ui.save_to_osu_textedit.text() + '/' + filename)
-                    for k in zip_file.namelist():
-                        zip_file.extract(k, self.ui.save_to_osu_textedit.text() + '/' + filename)
+            bp = list(set(self.bp))
+            map_id = bp[i]
+            idx = self.bp.index(map_id)
+            filename = str(map_id) + ' ' + self.composer[idx] + ' - ' + self.songname[idx]
+            filename = re.sub('[*?"<>]', '', filename)
+            filename = re.sub('[\/|~:]', '_', filename)
+            if '.' in filename:
+                filename = filename.replace('.', '')
+            url = 'https://txy1.sayobot.cn/download/osz/' + str(map_id)
+            #dir_ = '"' + self.ui.save_as_osz_textedit.text() + '"'
+            filename_a = '"' + filename + '.osz"'
+            with tempfile.TemporaryDirectory(dir=os.getcwd()) as tmpdir:
+                try:
+                    #os.system('.\\aria2c.exe -x2 -o ' + filename_a + ' -d "' + tmpdir + '" ' + url)
+                    res = subprocess.Popen('.\\aria2c.exe -x5 -o ' + filename_a + ' -d "' + tmpdir + '" ' + url, shell=True)
+                    r_code = res.wait()
+                    if self.ui.save_as_osz_checkbox.isChecked():
+                        print(os.listdir(tmpdir))
+                        copyfile(tmpdir + '\\' + filename + '.osz', self.ui.save_as_osz_textedit.text() + '/' + filename + '.osz')
+                    if self.ui.save_to_osu_checkbox.isChecked():
+                        if os.path.exists(self.ui.save_to_osu_textedit.text()) and not os.path.exists(self.ui.save_to_osu_textedit.text() + '/' + filename):
+                            os.mkdir(self.ui.save_to_osu_textedit.text() + '/' + filename)
+                            #unpack_archive(tmpdir + '\\' + filename + '.osz', self.ui.save_to_osu_textedit.text() + '/' + filename)
+                            os.system('.\\7z.exe x "' + tmpdir + '\\' + filename + '.osz" ' + '-o"' + self.ui.save_to_osu_textedit.text() + '/' + filename + '"')
+                except:
+                    pass
             self.done += 1
-            #self.ui.progressBar.setValue(self.done)
+            self.tasks -= 1
         else:
             pass
     
     def progress_display(self):
-        self.ui.progressBar.setValue(self.done)
+        self.ui.progressBar.setValue(round(100 * (self.done / len(list(set(self.bp))))))
+        if self.done == len(list(set(self.bp))):
+            ti = round(time.time() - self.t)
+            self.ui.down_status_label.setText('下载完成。共用时' + str(ti) + '秒')
+            self.done = 0
+            self.is_downloading = False
+        
 
 if __name__ == '__main__':
     app = QApplication([])
